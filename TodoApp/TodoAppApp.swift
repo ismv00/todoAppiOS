e432067b13786c9f68b374c1672790c6c0b75314
ismv00 <ismv00@icloud.com>
2025-05-04 22:18:24 -0400

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Igor S. Menezes on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
