cb52e283fa117fb20ec344b4e7ea04931e6774b9
ismv00 <ismv00@icloud.com>
2025-05-07 13:30:35 -0300

    feat:transition for loginview

--- TodoApp/HomeView.swift
+++ TodoApp/HomeView.swift
@@ -9,42 +9,45 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Image("main")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 250, height: 250)
-            
-            Text("Organize suas vida com o ToDo!")
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundStyle(.black)
-                .padding(.top, 20)
-            
-            Text("Crie, gerencie e conclua suas tarefas de forma prática. \n Mantenha seu dia em ordem com poucos toques.")
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.black)
-                .padding(.horizontal, 40)
-                .padding(.top, 10)
-            
-            Spacer()
-            
-            Button(action: {
-                //Aqui será a aÇão do botão
-            }) {
-                Text("Vamos começar")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color(hex: "#9DD4D7"))
+        
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("main")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+                
+                Text("Organize suas vida com o ToDo!")
+                    .font(.title2)
+                    .fontWeight(.bold)
                     .foregroundStyle(.black)
-                    .cornerRadius(10)
+                    .padding(.top, 20)
+                
+                Text("Crie, gerencie e conclua suas tarefas de forma prática. \n Mantenha seu dia em ordem com poucos toques.")
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.black)
                     .padding(.horizontal, 40)
+                    .padding(.top, 10)
+                
+                Spacer()
+                
+                NavigationLink(destination: LoginView()) {
+                    Text("Vamos começar")
+                        .fontWeight(.bold)
+                        .foregroundStyle(.black)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color(hex:"#9DD4D7"))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
             }
-            .padding(.bottom, 40)
+        
         }
-        .background(Color(hex: "#EEEEEE").ignoresSafeArea())
+        .background(Color(hex: "#EEEEEE"))
+        .ignoresSafeArea()
         
     }
 }
