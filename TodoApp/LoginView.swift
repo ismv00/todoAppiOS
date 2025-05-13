54cab7ed96a019ec41417239110ea90c207bfddb
ismv00 <ismv00@icloud.com>
2025-05-12 22:15:35 -0400

    feat: adiciona customBackgroundColor e ajustes no assets

--- TodoApp/LoginView.swift
+++ TodoApp/LoginView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("Bem vindo de volta!")
+                .font(.largeTitle).bold()
+                .foregroundStyle(.black)
+            Text("Vamos te ajudar a concluir suas tarefas")
+                .font(.subheadline)
+                .foregroundStyle(.black.opacity(0.7))
+            
+            Image("login")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+            
+            TextField("Digite seu e-mail", text: $email)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+            
+            TextField("Digite sua senha", text: $password)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+            
+            Button(action: {
+                           print("Esqueceu a senha")
+                       }) {
+                           Text("Esqueceu sua senha?")
+                               .foregroundColor(Color.customAccentColor)
+                               .padding(.bottom, 10)
+                       }
+            Button(action: {
+                print("Login")
+            }) {
+                Text("Login")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.customAccentColor)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+            
+            HStack {
+                Text("Não tem uma conta?")
+                Button(action: {
+                    print("Signup")
+                }) {
+                    Text("Signup")
+                        .foregroundStyle(Color.customAccentColor)
+                }
+            }
+            .padding(.bottom, 30)
+            
+            Spacer()
+        }
+        .background(Color.customBackgroundColor.ignoresSafeArea())
     }
 }
 
